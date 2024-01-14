b44dffe86018e9c1f106ccceac21e3d8a4da107e
edgar <edgardavid1503@gmail.com>
2024-01-13 19:44:48 -0800

    corrected server.js order

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ dotenv.config({ path: "./.env" });
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(
   cors({
     origin: "https://ec-sleepoutside.com", // Replace with your frontend's URL
@@ -26,15 +25,10 @@ app.use((req, res, next) => {
   res.header({
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
-  });
-  res.header({
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  });
-  res.header({
     "Access-Control-Allow-Origin": "https://ec-sleepoutside.com",
-  }),
-    res.header({ "Access-Control-Allow-Credentials": true });
-
+    "Access-Control-Allow-Credentials": true,
+  });
   next();
 });
 
